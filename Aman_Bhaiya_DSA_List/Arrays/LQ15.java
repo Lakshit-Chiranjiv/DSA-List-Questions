4e4d208a1620186de076718a5c63b3d6d1e4155b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-12 19:41:38 +0530

    lq15 method 1

---
+++ Aman_Bhaiya_DSA_List/Arrays/LQ15.java
@@ -0,0 +1,27 @@
+package Aman_Bhaiya_DSA_List.Arrays;
+
+import java.util.*;
+
+public class LQ15 {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.getOrDefault(nums[i],0) > 0)
+                return true;
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int elements: nums){
+            if(set.contains(elements))
+                return true;
+            else
+                set.add(elements);
+        }
+        return false;
+    }
+}
