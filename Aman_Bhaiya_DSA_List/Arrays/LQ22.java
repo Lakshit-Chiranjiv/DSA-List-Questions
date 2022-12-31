6d198d790d3c38546eab9f9afdcf814f2a504401
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-31 12:09:43 +0530

    LQ 22 java

---
+++ Aman_Bhaiya_DSA_List/Arrays/LQ22.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+        int mn, lmx = height[0], rmx = height[n-1], total = 0;
+        
+        left.add(lmx);
+        for(int i=1; i < n; i++){
+            lmx = Math.max(lmx,height[i]);
+            left.add(lmx);
+        }
+
+        right.add(rmx);
+        for(int i = (n-2); i>=0; i--){
+            rmx = Math.max(rmx,height[i]);
+            right.add(rmx);
+        }
+
+        for(int i = 0; i < n; i++){
+            mn = Math.min(left.get(i),right.get(n-i-1));
+            total = total + (mn - height[i]);
+        }
+
+        return total;
+    }
+}
