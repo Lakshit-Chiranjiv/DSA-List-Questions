2191e84ef48bbe8f5730b09229455b18cfe70393
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-03 20:56:10 +0530

    q20 py

---
+++ Striver/Recursion/q20.py
@@ -0,0 +1,27 @@
+# n-queen - bacltracking
+
+def nQueen(arr,col):
+    if col >= len(arr):
+        print(arr)
+        return
+    for i in range(len(arr)):
+        if isSafe(arr,i,col):
+            arr[i][col] = 1
+            nQueen(arr,col+1)
+            arr[i][col] = 0
+
+def isSafe(arr,row,col):
+    for i in range(col):
+        if arr[row][i] == 1:
+            return False
+    for i,j in zip(range(row,-1,-1),range(col,-1,-1)):
+        if arr[i][j] == 1:
+            return False
+    for i,j in zip(range(row,len(arr),1),range(col,-1,-1)):
+        if arr[i][j] == 1:
+            return False
+    return True
+
+
+arr = [[0 for i in range(4)] for j in range(4)]
+nQueen(arr,0)
