9ca2cada78c3fcc309c6f3ffd3d9e919173ef443
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-05 16:44:20 +0530

    2nd solution

--- Aman_Bhaiya_DSA_List/Arrays/LQ31.py
+++ Aman_Bhaiya_DSA_List/Arrays/LQ31.py
@@ -9,5 +9,18 @@ def mergeOverlappingIntervals(intervals):
             stack.append(intervals[i])
     return stack
 
+
+# in-place solution
+def mergeOverlappingIntervals(intervals):
+    intervals.sort(key = lambda x: x[0])
+    i = 0
+    while i < len(intervals)-1:
+        if intervals[i][1] >= intervals[i+1][0]:
+            intervals[i][1] = max(intervals[i][1], intervals[i+1][1])
+            intervals.pop(i+1)
+        else:
+            i += 1
+    return intervals
+
 intervals = [[1,3],[2,6],[8,10],[15,18]]
 print(mergeOverlappingIntervals(intervals))
