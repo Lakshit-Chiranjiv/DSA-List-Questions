cff24512472551b1a7c26b823f67842f74257944
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-09 19:00:22 +0530

    Q1 java dp

---
+++ Striver/DP/Q1.java
@@ -0,0 +1,51 @@
+package Striver.DP;
+
+public class Q1 {
+    // fibonacci number
+
+    //recursive solution
+    public static int fib(int n){
+        if(n==0 || n==1) return n;
+        return fib(n-1) + fib(n-2);
+    }
+
+    //memoization
+    public static int fib_memo(int n, int[] dp){
+        if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n] = fib_memo(n-1, dp) + fib_memo(n-2, dp);
+        return dp[n];
+    }
+
+    //tabulation
+    public static int fib_tab(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    //space optimization
+    public static int fib_sp(int n){
+        int a = 0, b = 1, c = 0;
+        for(int i=2; i<=n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int[] dp = new int[n+1];
+        for(int i=0; i<=n; i++) dp[i] = -1;
+        System.out.println(fib(n));
+        System.out.println(fib_memo(n, dp));
+        System.out.println(fib_tab(n));
+        System.out.println(fib_sp(n));
+    }
+}
