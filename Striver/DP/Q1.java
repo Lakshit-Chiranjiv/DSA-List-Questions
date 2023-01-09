cb4cd3505479211ee5da715febaa6e89e860fcd6
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-09 19:22:14 +0530

    Q1 java dp correction

--- Striver/DP/Q1.java
+++ Striver/DP/Q1.java
@@ -3,13 +3,13 @@ package Striver.DP;
 public class Q1 {
     // fibonacci number
 
-    //recursive solution
+    //recursive solution - Time complexity: O(2^n), Space complexity: O(n)
     public static int fib(int n){
         if(n==0 || n==1) return n;
         return fib(n-1) + fib(n-2);
     }
 
-    //memoization
+    //memoization - Time complexity: O(n), Space complexity: O(n)
     public static int fib_memo(int n, int[] dp){
         if(n==0 || n==1) return n;
         if(dp[n]!=-1) return dp[n];
@@ -17,7 +17,7 @@ public class Q1 {
         return dp[n];
     }
 
-    //tabulation
+    //tabulation - Time complexity: O(n), Space complexity: O(n)
     public static int fib_tab(int n){
         int[] dp = new int[n+1];
         dp[0] = 0;
@@ -28,7 +28,7 @@ public class Q1 {
         return dp[n];
     }
 
-    //space optimization
+    //space optimization - Time complexity: O(n), Space complexity: O(1)
     public static int fib_sp(int n){
         int a = 0, b = 1, c = 0;
         for(int i=2; i<=n; i++){
@@ -36,7 +36,7 @@ public class Q1 {
             a = b;
             b = c;
         }
-        return c;
+        return b;
     }
 
     public static void main(String[] args) {
