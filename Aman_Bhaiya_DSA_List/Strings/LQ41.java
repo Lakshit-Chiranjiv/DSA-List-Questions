d3916784846191746c221236e7c302fd313681bb
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-12 13:53:42 +0530

    LQ 41 java

---
+++ Aman_Bhaiya_DSA_List/Strings/LQ41.java
@@ -0,0 +1,22 @@
+package Aman_Bhaiya_DSA_List.Strings;
+
+public class LQ41 {
+    public boolean isAnagram(String s, String t) {
+        int[] occ = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            int idx = ((int)c) - 97;
+            occ[idx]++;
+        }
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            int idx = ((int)c) - 97;
+            occ[idx]--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(occ[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
