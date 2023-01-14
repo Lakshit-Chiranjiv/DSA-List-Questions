02e7d7e3d18d6ecbc7227d82b7f1dc75f7aff9b8
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-14 19:00:24 +0530

    Q6 py dp

---
+++ Striver/DP/Q6.py
@@ -0,0 +1,57 @@
+# Maximum Sum of Non-Adjacent Elements in cyclic manner | House Robber 2 problem
+
+# recursive solution
+def maxSum(arr, n):
+    if n == 0:
+        return arr[0]
+    if n < 0:
+        return 0
+    pick = arr[n] + maxSum(arr, n-2)
+    not_pick = maxSum(arr, n-1)
+    return max(pick, not_pick)
+
+# memoization
+def maxSumMemo(arr, n, dp):
+    if n == 0:
+        return arr[0]
+    if n < 0:
+        return 0
+    if dp[n] != -1:
+        return dp[n]
+    pick = arr[n] + maxSumMemo(arr, n-2, dp)
+    not_pick = maxSumMemo(arr, n-1, dp)
+    dp[n] = max(pick, not_pick)
+    return dp[n]
+
+# tabulation
+def maxSumTab(arr, n):
+    dp = [0 for i in range(n+1)]
+    dp[0] = arr[0]
+    for i in range(1, n+1):
+        pick = arr[i] + dp[i-2]
+        not_pick = dp[i-1]
+        dp[i] = max(pick, not_pick)
+    return dp[n]
+
+# space optimization
+def maxSumSpaceOpt(arr, n):
+    a = arr[0]
+    b = 0
+    for i in range(1, n+1):
+        pick = arr[i]
+        if i > 1:
+            pick += a
+        not_pick = b
+        c = max(pick, not_pick)
+        a = b
+        b = c
+    return b
+
+def maxSumCyclic(arr, n):
+    if n == 1:
+        return arr[0]
+    return max(maxSumSpaceOpt(arr[0:n-1], n-2), maxSumSpaceOpt(arr[1:], n-2))
+
+arr = [2, 3 , 2]
+n = len(arr)
+print(maxSumCyclic(arr, n))
