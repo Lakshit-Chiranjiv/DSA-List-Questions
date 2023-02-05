7024076327e6d7f12703fede1a4d17d3d6ad610c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-02-05 14:26:59 +0530

    feb 4 2023 app2

--- Leetcode Daily/Feb 2023/q4.py
+++ Leetcode Daily/Feb 2023/q4.py
@@ -41,3 +41,44 @@ def checkInclusion(self, s1: str, s2: str) -> bool:
         return True
 
     return False
+
+
+def checkInclusion2(self, s1: str, s2: str) -> bool:
+    mp = {}
+    c = len(s1)
+
+    if len(s1) > len(s2):
+        return False
+    if s1 == s2:
+        return True
+
+    for i in s1:
+        if mp.get(i,0) == 0:
+            mp[i] = 1
+        else:
+            mp[i] += 1
+
+    cmp = mp.copy()
+    
+    for i in range(len(s1)):
+        if cmp.get(s2[i],0) != 0:
+            mp[s2[i]] -= 1
+            if mp[s2[i]] >= 0:
+                c -= 1
+    
+    if c == 0:
+        return True
+    
+    for i in range(len(s1), len(s2)):
+        if cmp.get(s2[i],0) != 0:
+            mp[s2[i]] -= 1
+            if mp[s2[i]] >= 0:
+                c -= 1
+        if cmp.get(s2[i - len(s1)],0) != 0:
+            mp[s2[i - len(s1)]] += 1
+            if mp[s2[i - len(s1)]] > 0:
+                c += 1
+        if c == 0:
+            return True
+
+    return False
