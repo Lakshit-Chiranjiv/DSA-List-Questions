f3bf744f14570b70fc812a56116a81d2af879c40
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-02-11 12:57:53 +0530

    random lc q1342

---
+++ Leetcode Daily/Random Ques/q1342.py
@@ -0,0 +1,9 @@
+def numberOfSteps(self, num: int) -> int:
+    c = 0
+    while num != 0:
+        if num & 1 == 1:
+            num -= 1
+        else:
+            num //= 2
+        c += 1
+    return c
