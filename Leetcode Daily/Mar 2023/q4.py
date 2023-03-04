70af62c3494db7ea1b88a4686eebab5686737544
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-04 13:47:51 +0530

    mar 4 2023

---
+++ Leetcode Daily/Mar 2023/q4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        x = nums
+        n = len(nums)
+        for i in range(n):
+            if maxK < x[i] or x[i] < minK:
+                x[i] = -1
+        minK_found = 0
+        maxK_found = 0
+        wind_left = 0
+        wind_right = 0
+        last_minK_idx = -1
+        last_maxK_idx = -1
+        ans = 0
+        while wind_right < n:
+            if x[wind_right] == -1:
+                minK_found = 0
+                maxK_found = 0
+                last_minK_idx = -1
+                last_maxK_idx = -1
+                wind_left = wind_right + 1
+            if minK <= x[wind_right] <= maxK:
+                if x[wind_right] == minK:
+                    minK_found = 1
+                    last_minK_idx = wind_right
+                if x[wind_right] == maxK:
+                    maxK_found = 1
+                    last_maxK_idx = wind_right
+            if minK_found == 1 and maxK_found == 1:
+                ans += (min(last_minK_idx,last_maxK_idx) - wind_left + 1)
+            wind_right += 1
+
+        return ans
