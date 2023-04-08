dcdfbf22654e871a4b12b3dfb17a0260cbc94952
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-08 12:24:49 +0530

    Q55 py dp

---
+++ Striver/DP/Q55.py
@@ -0,0 +1,34 @@
+# maximum rectangle area with all 1's in a matrix
+
+def largestRectangleArea(heights):
+    st = []
+    mxa = 0
+    n = len(heights)
+
+    for i in range(n+1):
+        while st and (i == n or heights[st[-1]] >= heights[i]):
+            ht = heights[st[-1]]
+            st.pop()
+            wd = 0
+            if len(st)==0: wd = i
+            else:
+                wd = i - st[-1] - 1
+            mxa = max(mxa,wd*ht)
+        st.append(i)
+    return mxa
+
+def maximalRectangle(matrix):
+    if len(matrix)==0: return 0
+    m = len(matrix)
+    n = len(matrix[0])
+    heights = [0]*n
+    mxa = 0
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j]=='1': heights[j] += 1
+            else: heights[j] = 0
+        mxa = max(mxa,largestRectangleArea(heights))
+    return mxa
+
+matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+print(maximalRectangle(matrix))
