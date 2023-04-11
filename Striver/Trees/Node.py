f3c2e0ba87a95166b6ade6162e1980d6439d9d6a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-11 09:45:07 +0530

    Node python class

---
+++ Striver/Trees/Node.py
@@ -0,0 +1,10 @@
+# node class
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.data)
