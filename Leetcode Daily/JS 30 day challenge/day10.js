eadf827f5e9134f06e2f4a844411c40f2006c184
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-05-15 22:40:43 +0530

    js 30 day challenge - day 10

---
+++ Leetcode Daily/JS 30 day challenge/day10.js
@@ -0,0 +1,10 @@
+var curry = function(fn) {
+
+    return function curried(...args) {
+    if (args.length >= fn.length) {
+        return fn(...args)
+    }
+
+    return (...nextArgs) => curried(...args,...nextArgs)
+  }
+};
