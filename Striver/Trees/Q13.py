1df9aeff682e59f8cf2df8afb6e216971ec9c05d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-05-25 11:20:45 +0530

    Q13 py trees

---
+++ Striver/Trees/Q13.py
@@ -0,0 +1,31 @@
+# max path sum from any node to any node
+
+from Node import Node
+
+def maxPathSum(root, res):
+    if not root:
+        return 0
+    l = maxPathSum(root.left, res)
+    r = maxPathSum(root.right, res)
+    temp = max(max(l, r) + root.val, root.val)
+    ans = max(temp, l + r + root.val)
+    res[0] = max(res[0], ans)
+    return temp
+
+def maxPathSumUtil(root):
+    if not root:
+        return 0
+    res = [float('-inf')]
+    maxPathSum(root, res)
+    return res[0]
+
+root = Node(10)
+root.left = Node(2)
+root.right = Node(10)
+root.left.left = Node(20)
+root.left.right = Node(1)
+root.right.right = Node(-25)
+root.right.right.left = Node(3)
+root.right.right.right = Node(4)
+
+print(maxPathSumUtil(root))
