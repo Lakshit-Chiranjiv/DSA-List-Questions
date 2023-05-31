ee24c25cb7dd72c416f388734e5fe41b01a24c1e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-05-31 11:02:30 +0530

    js 30 day challenge - day 27

--- Leetcode Daily/JS 30 day challenge/day27.js
+++ Leetcode Daily/JS 30 day challenge/day27.js
@@ -1,32 +1,28 @@
 class EventEmitter {
-    eventMap = {}
-  subscribe(event, cb) {
-    if (!this.eventMap.hasOwnProperty(event))
-        this.eventMap[event] = new Set()
-    this.eventMap[event].add(cb)
-    
-    return {
+    constructor() {
+      this.events = {};
+    }
+  
+    subscribe(event, cb) {
+      if (!(event in this.events)) {
+        this.events[event] = new Set([cb]);
+      } else {
+        this.events[event].add(cb);
+      }
+  
+      return {
         unsubscribe: () => {
-            this.eventMap[event].delete(cb)
-        }
-    };
+          this.events[event].delete(cb);
+        },
+      };
+    }
+  
+    emit(event, args = []) {
+      if (!(event in this.events)) return [];
+      const result = [];
+      this.events[event].forEach((fn) => {
+        result.push(fn(...args));
+      });
+      return result;
+    }
   }
-
-  emit(event, args = []) {
-      const res = []
-      (this.eventMap[event] ?? []).forEach((cb) => res.push(cb(...args)))
-      return res
-  }
-}
-
-/**
- * const emitter = new EventEmitter();
- *
- * // Subscribe to the onClick event with onClickCallback
- * function onClickCallback() { return 99 }
- * const sub = emitter.subscribe('onClick', onClickCallback);
- *
- * emitter.emit('onClick'); // [99]
- * sub.unsubscribe(); // undefined
- * emitter.emit('onClick'); // []
- */
