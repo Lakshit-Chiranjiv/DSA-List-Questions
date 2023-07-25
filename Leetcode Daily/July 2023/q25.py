195db66dee6a26978aa73bad8111645dd4f3f60a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-07-25 08:50:57 +0530

    july 25 2023

---
+++ Leetcode Daily/July 2023/q25.py
@@ -0,0 +1,15 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n = len(arr)
+        s = 0
+        e = n-1
+        while s < e:
+            m = (s+e)//2
+            print(m)
+            if arr[m] > arr[m-1] and arr[m] > arr[m+1]:
+                return m
+            elif arr[m] > arr[m-1]:
+                s = m+1
+            else:
+                e = m
+        return e
