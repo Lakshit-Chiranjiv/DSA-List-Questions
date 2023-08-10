f56d3f92a10d1bc7a08d9304b48f63ebd18b8743
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-08-10 23:21:28 +0530

    aug 10 2023

---
+++ Leetcode Daily/Aug 2023/q10.py
@@ -0,0 +1,31 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        bi = -1
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                bi = i
+                break
+        
+        s = 0
+        e = bi
+        while s <= e:
+            m = (s + e) // 2
+            if nums[m] == target:
+                return True
+            elif nums[m] < target:
+                s = m + 1
+            else:
+                e = m - 1
+        
+        s = bi + 1
+        e = len(nums) - 1
+        while s <= e:
+            m = (s + e) // 2
+            if nums[m] == target:
+                return True
+            elif nums[m] < target:
+                s = m + 1
+            else:
+                e = m - 1
+        
+        return False
