e88ad03fbd1cf8ed746d6b5e3b94df27fb5df0f9
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-09-30 14:13:33 +0530

    sept 30 2023

---
+++ Leetcode Daily/Sept 2023/q30.py
@@ -0,0 +1,13 @@
+class Solution:
+    def find132pattern(self, nums):
+        third = float('-inf')
+        stack = []
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] < third:
+                return True
+            while stack and stack[-1] < nums[i]:
+                third = stack[-1]
+                stack.pop()
+            stack.append(nums[i])
+        
+        return False
