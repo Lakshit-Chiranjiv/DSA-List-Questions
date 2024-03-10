9072ba06d8af8dc4f5b7138db38cb5771f14e482
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2024-03-10 09:36:55 +0530

    mar 10 2024

--- Leetcode Daily/Mar 2024/q10.py
+++ Leetcode Daily/Mar 2024/q10.py
@@ -1,3 +1,9 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1).intersection(set(nums2)))
+
+# 349. Intersection of Two Arrays
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+    
+# We use the intersection method to find the common elements between the two lists. We then convert the set to a list and return it.
